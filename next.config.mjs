551aa1ddfe5eb3e51e1bac92473bd0ed985b5ab8
daviljr <davi.cool@hotmail.com>
2025-12-01 01:44:37 -0300

    fix(next): remove git conflict markers from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-  reactStrictMode: true
-=======
   reactStrictMode: true,
->>>>>>> 8bf97ec (fix: remove experimental.appDir (compatibilidade Next.js 14))
 };
 export default nextConfig;
