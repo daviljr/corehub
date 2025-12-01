c8ed58158a35705c76afd7f9768dc66a01407e7e
daviljr <davi.cool@hotmail.com>
2025-12-01 01:33:09 -0300

    fix: remove experimental.appDir (compatibilidade Next.js 14)

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+<<<<<<< HEAD
   reactStrictMode: true
+=======
+  reactStrictMode: true,
+>>>>>>> 8bf97ec (fix: remove experimental.appDir (compatibilidade Next.js 14))
 };
 export default nextConfig;
